d319c0c60743228e2b96119020afd5322f06aedf
Linus Eklind <76260367+Eklinden@users.noreply.github.com>
2022-09-07 14:01:15 +0200

    added arrow under hem

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,11 +5,30 @@ const Hero = () => {
   return (
     <section className="hero">
       <h1>
-        Varje djur förtjänar <br></br> ett <span>hem</span>
+        Varje djur förtjänar <br></br>{" "}
+        <div>
+          ett
+          <span>
+            hem
+            <svg
+              width="144"
+              height="25"
+              viewBox="0 0 144 25"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M1.21077 15.99C24.8022 8.41801 143.752 1.60663 143.752 1.60663C117.426 6.08828 63.6136 16.9149 58.9694 24.3682"
+                stroke="#5E9FFF"
+                stroke-width="2"
+              />
+            </svg>
+          </span>
+        </div>
       </h1>
-      {/* <AnimalCard />
-      <AnimalCard />
-      <AnimalCard /> */}
+      <div className="test"></div>
+      <div className="test"></div>
+      <div className="test"></div>
       <article>
         <p></p>
       </article>
