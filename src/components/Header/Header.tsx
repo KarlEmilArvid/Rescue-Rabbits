dde0a80c236ec90e619646a6e42cf8e65486c371
Linus Eklind <76260367+Eklinden@users.noreply.github.com>
2022-09-07 10:15:29 +0200

    created assets and css header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,11 @@
+import logo from "../../assets/logo.svg";
 import "./Header.scss";
 
 const Header = () => {
   return (
     <header className="header">
       <figure>
-        <img src="" alt="" />
+        <img src={logo} alt="" />
       </figure>
       <h1>Rescue Rabbits</h1>
       <a href="">Kontakta oss</a>
