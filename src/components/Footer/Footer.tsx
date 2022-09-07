a93de3aab764d34aca1694ab8470c70c509bba95
Ida Larsson <77833853+IdaLarsson90@users.noreply.github.com>
2022-09-07 10:36:39 +0200

    Footer added

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,8 @@
 import "./Footer.scss";
+import facebook from "../../assets/facebook.svg"
+import twitter from "../../assets/twitter.svg"
+import insta from "../../assets/insta.svg"
+import linkedin from "../../assets/linkedin.svg"
 
 const Footer = () => {
   return (
@@ -9,20 +13,22 @@ const Footer = () => {
         <p>204 24 Åkersberga</p>
         <p className="phone">010-657 674</p>
       </section>
-      <section>
+      <section className="social-media">
         <h2>Sociala Medier</h2>
-        <a href="">
-          <img src="" alt="" />
-        </a>
-        <a href="">
-          <img src="" alt="" />
-        </a>
-        <a href="">
-          <img src="" alt="" />
-        </a>
-        <a href="">
-          <img src="" alt="" />
-        </a>
+        <div>
+          <a href="">
+            <img src={twitter} alt="twitter icon" />
+          </a>
+          <a href="">
+            <img src={insta} alt="instagram icon" />
+          </a>
+          <a href="">
+            <img src={facebook} alt="facebook icon" />
+          </a>
+          <a href="">
+            <img src={linkedin} alt="linked icon" />
+          </a>
+        </div>
       </section>
       <section>
         <h2>Kundtjänst</h2>
