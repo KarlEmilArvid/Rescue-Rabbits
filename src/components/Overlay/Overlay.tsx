62d4c3d7fea1addf06b25b14957df67b15ac53fd
Ida Larsson <77833853+IdaLarsson90@users.noreply.github.com>
2022-09-08 10:26:34 +0200

    pill med hero

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -1,42 +1,34 @@
 import "./Overlay.scss";
 import Breadcrumbs from "../Breadcrumbs/Breadcrumbs"
 import close from "../../assets/close.svg"
-import locationPin from "../../assets/locationPin.svg"
+
 import { Animal } from "../../models/data";
+import AnimalInfo from "./AnimalInfo";
 
 interface Props {
   animals: Animal[];
   //    handleClick:
 }
 const Overlay = ({ animals }: Props) => {
-  console.log(animals)
-
+  
+  const theAnimal = animals[10]
+  console.log(theAnimal)
   return (
     <section className="overlay">
       <section className="overlay__container">
         <header className="overlay__header">
           <Breadcrumbs />
           <figure>
-            <img src={close} alt="" />
+            <img src={close} alt="close button" />
           </figure>
         </header>
         <main className="overlay__main">
-          <img className="animalInfo__img" src="https://images.unsplash.com/photo-1552053831-71594a27632d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZG9nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" alt="" />
-          <section className="animalInfo">
-            <h2 className="animalInfo__name">Lossan</h2>
-            <p className="animalInfo__age">1 år</p>
-            <div className="animalInfo__location">
-              <figure className="animalInfo__locationPin">
-                <img className="animalInfo__locationPin" src={locationPin} alt="" />
-              </figure>
-              <p className="animalInfo__place">Karlstad</p>
-            </div>
-            <p className="animalInfo__description">Puppy kitty ipsum dolor sit good dog foot stick canary. Teeth Mittens grooming vaccine walk swimming nest good boy furry tongue heel furry treats fish. E toys polydactyl meow.</p>
-          </section>
+          <img className="animalInfo__img" src={theAnimal.image} alt="" />
+          <AnimalInfo theAnimal={theAnimal}/>
         </main>
         <footer className="overlay__footer">
-          <p className="interestedCount">X personer är intresserade av Y</p>
-          <button className="button">Intresseanmälan</button>
+          <p className="interestedCount">X personer är intresserade av {theAnimal.name}</p>
+          <button className="button button--overlay">Intresseanmälan</button>
         </footer>
       </section>
     </section>
