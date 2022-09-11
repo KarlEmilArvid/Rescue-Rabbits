db363350ae6f2b9cf85ac946eb513c39c650bc42
KarlEmilArvid <emilarvidsson96@gmail.com>
2022-09-11 17:37:43 +0200

    Temporary Hero card fix

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,6 @@
 import { Animal } from "../../models/data";
 import AnimalCard from "../AnimalCard/AnimalCard";
+import map from '../../assets/MapPin.svg'
 import "./Hero.scss";
 
 interface Props {
@@ -7,7 +8,8 @@ interface Props {
   setPickedAnimal: (pickedAnimal: Animal) => void;
   pickedAnimal: Animal;
 }
-
+//Hero är temporärt fixat, tror ej vi kommer kunna visa kort i vilket fall när vi har sök funktionen, söker vi på fågel så kommer alla katter i heron att försvinna tex
+//Blir nog lättast att ha tre statiska djur kort, bättre att lägga fokus på sök funktion och responsiv styling
 const Hero = ({ animals, pickedAnimal, setPickedAnimal }: Props) => {
 //  let firstThreeInAnimals = animals.slice(0, 3);
   return (
@@ -34,7 +36,48 @@ const Hero = ({ animals, pickedAnimal, setPickedAnimal }: Props) => {
           </span>
         </div>
       </h1>
-
+      <section className='card'>
+        <img className='animal-image' src="https://images.unsplash.com/photo-1596854407944-bf87f6fdd49e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+        <section className='text-block'>
+          <h2 className='name'>Pär</h2>
+          <section className="location">
+            <p>
+              <img className='map-pin' src={map} alt='map pin'/>Umeå
+            </p>
+          </section>
+          <p className='age'> 4 år</p>
+          <p className='date'>Letat efter ett hem sedan 2018-06-12</p>
+          <button className='card-button'>Läs mer</button>
+        </section>
+      </section>
+      <section className='card'>
+        <img className='animal-image' src="https://images.unsplash.com/photo-1596854407944-bf87f6fdd49e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+        <section className='text-block'>
+          <h2 className='name'>Pär</h2>
+          <section className="location">
+            <p>
+              <img className='map-pin' src={map} alt='map pin'/>Umeå
+            </p>
+          </section>
+          <p className='age'> 4 år</p>
+          <p className='date'>Letat efter ett hem sedan 2018-06-12</p>
+          <button className='card-button'>Läs mer</button>
+        </section>
+      </section>
+      <section className='card'>
+        <img className='animal-image' src="https://images.unsplash.com/photo-1596854407944-bf87f6fdd49e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
+        <section className='text-block'>
+          <h2 className='name'>Pär</h2>
+          <section className="location">
+            <p>
+              <img className='map-pin' src={map} alt='map pin'/>Umeå
+            </p>
+          </section>
+          <p className='age'> 4 år</p>
+          <p className='date'>Letat efter ett hem sedan 2018-06-12</p>
+          <button className='card-button'>Läs mer</button>
+        </section>
+      </section>
       <article>
         <p></p>
       </article>
@@ -42,10 +85,10 @@ const Hero = ({ animals, pickedAnimal, setPickedAnimal }: Props) => {
         <img src="" alt="" />
       </figure>
     </section>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
 
 
 //      {firstThreeInAnimals.map((animals, pickedAnimal, setPickedAnimal) => (
