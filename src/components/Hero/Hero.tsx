f787a0df6c1a9147b7b401f74b1a57f9565bd1d2
Linus Eklind <76260367+Eklinden@users.noreply.github.com>
2022-09-12 11:15:50 +0200

    Update Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,7 +1,7 @@
-import { Animal } from "../../models/data"
-import AnimalCard from "../AnimalCard/AnimalCard"
-import map from '../../assets/MapPin.svg'
-import "./Hero.scss"
+import map from "../../assets/MapPin.svg";
+import { Animal } from "../../models/data";
+import AnimalCard from "../AnimalCard/AnimalCard";
+import "./Hero.scss";
 
 interface Props {
   animals: Animal[];
@@ -11,7 +11,7 @@ interface Props {
 //Hero är temporärt fixat, tror ej vi kommer kunna visa kort i vilket fall när vi har sök funktionen, söker vi på fågel så kommer alla katter i heron att försvinna tex
 //Blir nog lättast att ha tre statiska djur kort, bättre att lägga fokus på sök funktion och responsiv styling
 const Hero = ({ animals, pickedAnimal, setPickedAnimal }: Props) => {
-//  let firstThreeInAnimals = animals.slice(0, 3);
+  //  let firstThreeInAnimals = animals.slice(0, 3);
   return (
     <section className="hero">
       <h1>
@@ -36,59 +36,79 @@ const Hero = ({ animals, pickedAnimal, setPickedAnimal }: Props) => {
           </span>
         </div>
       </h1>
-      <section className='card'>
-        <img className='animal-image' src="https://images.unsplash.com/photo-1569591159212-b02ea8a9f239?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
-        <section className='text-block'>
-          <h2 className='name'>Kent</h2>
+      <section className="card">
+        <img
+          className="animal-image"
+          src="https://images.unsplash.com/photo-1569591159212-b02ea8a9f239?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjd8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+        />
+        <section className="text-block">
+          <h2 className="name">Kent</h2>
           <section className="location">
             <p>
-              <img className='map-pin' src={map} alt='map pin'/>Eskilstuna
+              <img className="map-pin" src={map} alt="map pin" />
+              Eskilstuna
             </p>
           </section>
-          <p className='age'> 32 år</p>
-          <p className='date'>Letat efter ett hem sedan 2010-12-24</p>
-          <button className='card-button'>Läs mer</button>
+          <p className="age"> 32 år</p>
+          <p className="date">Letat efter ett hem sedan 2010-12-24</p>
+          <button className="card-button">Läs mer</button>
         </section>
       </section>
-      <section className='card'>
-        <img className='animal-image' src="https://images.unsplash.com/photo-1596854407944-bf87f6fdd49e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"/>
-        <section className='text-block'>
-          <h2 className='name'>Pär</h2>
+      <section className="card">
+        <img
+          className="animal-image"
+          src="https://images.unsplash.com/photo-1596854407944-bf87f6fdd49e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGNhdHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+        />
+        <section className="text-block">
+          <h2 className="name">Pär</h2>
           <section className="location">
             <p>
-              <img className='map-pin' src={map} alt='map pin'/>Umeå
+              <img className="map-pin" src={map} alt="map pin" />
+              Umeå
             </p>
           </section>
-          <p className='age'> 4 år</p>
-          <p className='date'>Letat efter ett hem sedan 2018-06-12</p>
-          <button className='card-button'>Läs mer</button>
+          <p className="age"> 4 år</p>
+          <p className="date">Letat efter ett hem sedan 2018-06-12</p>
+          <button className="card-button">Läs mer</button>
         </section>
       </section>
-      <section className='card'>
-        <img className='animal-image' src="https://images.unsplash.com/photo-1618252903592-2b8c90c43a6e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z3VpbmVhJTIwcGlnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"/>
-        <section className='text-block'>
-          <h2 className='name'>Åke</h2>
+      <section className="card">
+        <img
+          className="animal-image"
+          src="https://images.unsplash.com/photo-1618252903592-2b8c90c43a6e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z3VpbmVhJTIwcGlnfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+        />
+        <section className="text-block">
+          <h2 className="name">Åke</h2>
           <section className="location">
             <p>
-              <img className='map-pin' src={map} alt='map pin'/>Karlskoga
+              <img className="map-pin" src={map} alt="map pin" />
+              Karlskoga
             </p>
           </section>
-          <p className='age'> 13 år</p>
-          <p className='date'>Letat efter ett hem sedan 2020-01-22</p>
-          <button className='card-button'>Läs mer</button>
+          <p className="age"> 13 år</p>
+          <p className="date">Letat efter ett hem sedan 2020-01-22</p>
+          <button className="card-button">Läs mer</button>
         </section>
       </section>
       <article>
-        <p></p>
+        <p>
+          Rescue Rabbit är en plats där djur kan omplaceras. Vi tror på starkt
+          på förtroende mellan parter, därför låter vi alltid den nuvarande
+          ägaren bestämma själv till vilken av de intresserade parter hen vill
+          adoptera bort djuret till. Vi kämpar för att varje djur ska ha en
+          trygg och hälsosam framtid. Det gör vi genom att skicka ut våra
+          vetrinärer och göra en hälsokontroll på djuret innan det kommer upp på
+          hemsidan. Scrolla ned för att se alla djur
+        </p>
       </article>
       <figure>
         <img src="" alt="" />
       </figure>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
 
 //      {firstThreeInAnimals.map((animals, pickedAnimal, setPickedAnimal) => (
 //  <AnimalCard animal={animals} pickedAnimal={pickedAnimal} setPickedAnimal={setPickedAnimal}/>
