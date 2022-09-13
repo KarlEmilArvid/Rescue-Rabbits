40793edd1b4b7ff4f96929e0c81eb118030930be
Ida Larsson <77833853+IdaLarsson90@users.noreply.github.com>
2022-09-13 09:55:33 +0200

    hola

--- src/components/AnimalCard/AnimalCard.tsx
+++ src/components/AnimalCard/AnimalCard.tsx
@@ -1,14 +1,7 @@
-<<<<<<< Updated upstream
-import { Animal } from '../../models/data'
-import './AnimalCard.scss'
-import map from '../../assets/MapPin.svg'
-import arrow from '../../assets/Arrow.svg'
-=======
 import arrow from "../../assets/Arrow.svg";
 import map from "../../assets/MapPin.svg";
 import { Animal } from "../../models/data";
 import "./AnimalCard.scss";
->>>>>>> Stashed changes
 
 interface Props {
   animal: Animal;
@@ -17,23 +10,18 @@ interface Props {
   showOverlay: () => void;
 }
 
-<<<<<<< Updated upstream
-const AnimalCard = ({ showOverlay, animal, pickedAnimal, setPickedAnimal }: Props) => {
-=======
 const AnimalCard = ({
   showOverlay,
   animal,
   pickedAnimal,
   setPickedAnimal,
 }: Props) => {
->>>>>>> Stashed changes
   function handleClick() {
     showOverlay();
     setPickedAnimal(animal);
   }
 
   return (
-<<<<<<< Updated upstream
     <section onClick={handleClick} className='card'>
       <img className='animal-image' src={animal.image} />
       {animal.adopted ? (
@@ -58,27 +46,3 @@ const AnimalCard = ({
 }
 
 export default AnimalCard
-=======
-    <section onClick={handleClick} className="card">
-      <img className="animal-image" src={animal.image} />
-      <section className="text-block">
-        <h2 className="name">{animal.name}</h2>
-        <section className="location">
-          <img className="map-pin" src={map} alt="map pin" />
-          <p>{animal.location}</p>
-        </section>
-        <p className="age"> {animal.age} år</p>
-        <p className="date">
-          Letat efter ett hem sedan <br /> 2018-06-12
-        </p>
-        <button className="card-button">
-          Läs mer
-          <img src={arrow} />
-        </button>
-      </section>
-    </section>
-  );
-};
-// {animal.dateAdded}
-export default AnimalCard;
->>>>>>> Stashed changes
--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -11,31 +11,21 @@ interface Props {
   setPickedAnimal: (pickedAnimal: Animal) => void;
   pickedAnimal: Animal;
   visibility: string;
-  // ÄNDRA TYPER!!!!
-<<<<<<< HEAD
   hideOverlay: () => void;
   setStep: (num:number) => void;
-=======
-  hideOverlay: any;
-<<<<<<< Updated upstream
-  setStep: any;
->>>>>>> 69fee15610af90bd8a75410e4fb5b38f7994ae03
   step: number;
 }
 
-const Overlay = ({ step, setStep, visibility, hideOverlay, setPickedAnimal, pickedAnimal }: Props) => {
-=======
-}
 
 const Overlay = ({
   visibility,
   hideOverlay,
   setPickedAnimal,
   pickedAnimal,
-}: Props) => {
+  }: Props) => {
   const [step, setStep] = useState<number>(1);
 
->>>>>>> Stashed changes
+
   function handleClick() {
     if (step === 3) {
       hideOverlay();
@@ -60,44 +50,19 @@ const Overlay = ({
           <header className="overlay__header">
             <Breadcrumbs step={step} />
             <figure>
-<<<<<<< Updated upstream
               <img src={close} alt="close button" onClick={() => { handleClose() }} />
-=======
-              <img
-                src={close}
-                alt="close button"
-                onClick={() => {
-                  handleClose();
-                }}
-              />
->>>>>>> Stashed changes
             </figure>
           </header>
           <main className="overlay__main overlay__main--column">
             <div>
-<<<<<<< Updated upstream
               <img className="animalInfo__img" src={pickedAnimal.image} alt="" />
-=======
-              <img
-                className="animalInfo__img"
-                src={pickedAnimal.image}
-                alt=""
-              />
->>>>>>> Stashed changes
               <h2 className="animalInfo__name">{pickedAnimal.name}</h2>
             </div>
             <Form handleClick={handleClick} />
           </main>
-<<<<<<< Updated upstream
         </section>
       </section>
     )
-=======
-          <footer className="overlay__footer"></footer>
-        </section>
-      </section>
-    );
->>>>>>> Stashed changes
   } else if (step === 3) {
     //Bekräftat
     return (
@@ -106,57 +71,21 @@ const Overlay = ({
           <header className="overlay__header">
             <Breadcrumbs step={step} />
             <figure>
-<<<<<<< Updated upstream
               <img src={close} alt="close button" onClick={() => { handleClose() }} />
             </figure>
           </header>
           <main className="overlay__main overlay__main--column">
-=======
-              <img
-                src={close}
-                alt="close button"
-                onClick={() => {
-                  handleClose();
-                }}
-              />
-            </figure>
-          </header>
-          <main className="overlay__main overlay__main--column">
-            <div>
-              <img
-                className="animalInfo__img"
-                src={pickedAnimal.image}
-                alt=""
-              />
-              <h2 className="animalInfo__name">{pickedAnimal.name}</h2>
-            </div>
->>>>>>> Stashed changes
             <figure className="confirmedGif">
               <img src={confetti} alt="confetti" />
             </figure>
             <p>Tack för visat intresse. Vi hör av oss så fort vi kan.</p>
           </main>
           <footer className="overlay__footer overlay__footer--center">
-<<<<<<< Updated upstream
             <button onClick={() => { handleClick() }} className="button-overlay button-center">Toppen!</button>
           </footer>
         </section>
       </section>
     )
-=======
-            <button
-              onClick={() => {
-                handleClick();
-              }}
-              className="button-center"
-            >
-              Toppen!
-            </button>
-          </footer>
-        </section>
-      </section>
-    );
->>>>>>> Stashed changes
   } else {
     return (
       //läs mer om djuret
@@ -164,21 +93,12 @@ const Overlay = ({
         <section className="overlay__container">
           <header className="overlay__header">
             <Breadcrumbs step={step} />
-<<<<<<< Updated upstream
             <figure onClick={() => { handleClose() }}>
-=======
-            <figure
-              onClick={() => {
-                handleClose();
-              }}
-            >
->>>>>>> Stashed changes
               <img src={close} alt="close button" />
             </figure>
           </header>
           <main className="overlay__main">
             <img className="animalInfo__img" src={pickedAnimal.image} alt="" />
-<<<<<<< Updated upstream
             <AnimalDescription pickedAnimal={pickedAnimal} setPickedAnimal={setPickedAnimal} />
           </main>
           {pickedAnimal.adopted ? (<></>) :
@@ -189,53 +109,7 @@ const Overlay = ({
         </section>
       </section>
     )
-=======
-            <AnimalDescription
-              pickedAnimal={pickedAnimal}
-              setPickedAnimal={setPickedAnimal}
-            />
-          </main>
-          <footer className="overlay__footer">
-            <p className="interestedCount">
-              X personer är intresserade av {pickedAnimal.name}
-            </p>
-            <button
-              onClick={() => {
-                handleClick();
-              }}
-              className="button-overlay"
-            >
-              Intresseanmälan
-            </button>
-          </footer>
-        </section>
-      </section>
-    );
->>>>>>> Stashed changes
   }
 };
 
-<<<<<<< Updated upstream
 export default Overlay
-
-//   <section className={visibility}>
-//   <section className="overlay__container">
-//     <header className="overlay__header">
-//       <Breadcrumbs step={step}/>
-//       <figure onClick={()=> {handleClose()}}>
-//         <img src={close} alt="close button"/>
-//       </figure>
-//     </header>
-//     <main className="overlay__main">
-//       <img className="animalInfo__img" src={pickedAnimal.image} alt=""/>
-//       <AnimalDescription pickedAnimal={pickedAnimal} setPickedAnimal={setPickedAnimal}/>
-//     </main>
-//     <footer className="overlay__footer">
-//       <p className="interestedCount">X personer är intresserade av {pickedAnimal.name}</p>
-//       <button onClick={()=> {handleClick()}} className="button-overlay">Intresseanmälan</button>
-//     </footer>
-//   </section>
-// </section>
-=======
-export default Overlay;
->>>>>>> Stashed changes
