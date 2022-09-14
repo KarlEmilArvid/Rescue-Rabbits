e4b4961b4efef348cd020b9b48d8f59ca39a3c83
Linus Eklind <76260367+Eklinden@users.noreply.github.com>
2022-09-14 15:10:32 +0200

    updated load more button

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import searchIcon from "../../assets/searchIcon.svg"
 import { Animal } from "../../models/data"
 import AnimalCard from "../AnimalCard/AnimalCard"
@@ -15,9 +15,27 @@ const Main = ({ showOverlay, animals, setPickedAnimal, pickedAnimal }: Props) =>
   const [query, setQuery] = useState<string>("")
   const cardsShown = 6; //visar 6 kort, 6 mer vid varje knapptryck, när alla kort är visade så försvinner knappen
   const [load, setLoad] = useState<number>(cardsShown)
+  const [animalsToShow, setanimalsToShow] = useState<Animal[]>(animals.filter(animal => animal.type.toLowerCase().includes(query) || animal.location.toLowerCase().includes(query)))
+  const [showButton, setshowButton] = useState<boolean>(true)
   const loadMoreCards: () => void = () => {
     setLoad(load + cardsShown);
+    setanimalsToShow(animals.filter(animal => animal.type.toLowerCase().includes(query) || animal.location.toLowerCase().includes(query)));
   }
+
+  useEffect(() => {
+    setanimalsToShow(animals.filter(animal => animal.type.toLowerCase().includes(query) || animal.location.toLowerCase().includes(query)));
+    setLoad(cardsShown)
+  }, [query]);
+
+  useEffect(() => {
+    if (animalsToShow.length == load || animalsToShow.length < load) {
+      setshowButton(false)
+    } else {
+      setshowButton(true)
+    }
+    console.log(load)
+  }, [load]);
+  
 
   return (
     <main className="main">
@@ -31,14 +49,18 @@ const Main = ({ showOverlay, animals, setPickedAnimal, pickedAnimal }: Props) =>
           <img className="search-icon" src={searchIcon} alt="" />
         </section>
       </section>
-      <section className="animal-list">
+      <section id="animal-list">
         {animals?.filter((animal) =>
           animal.type.toLowerCase().includes(query) || animal.location.toLowerCase().includes(query)).slice(0, load)?.map((animal) => (
             <AnimalCard showOverlay={showOverlay} animal={animal} pickedAnimal={pickedAnimal} setPickedAnimal={setPickedAnimal} key={animal.id} />
           ))}
       </section>
       <div className="button-wrapper">
-        {load < animals?.length && (<button onClick={loadMoreCards}>Läs in fler</button>)}
+        {showButton ? (
+          <button onClick={loadMoreCards}>Läs in fler</button>
+        ):(<></>)}
+        {/* {showButton === 6 || 12 || 18 || 24 && (<button onClick={loadMoreCards}>Läs in fler</button>)} */}
+        {/* {load < animals?.length && (<button onClick={loadMoreCards}>Läs in fler</button>)} */}
       </div>
     </main>
   );
